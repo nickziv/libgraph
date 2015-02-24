30dfbd8b20b264b5ef23d338141ce71ff36918c7
Nick Zivkovic <zivkovic.nick@gmail.com>
2015-02-24 05:08:12 +0000

    Add appropriate license and headers.

--- src/graph_umem.c
+++ src/graph_umem.c
@@ -1,26 +1,13 @@
 /*
- * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").
- * You may not use this file except in compliance with the License.
- *
- * You can obtain a copy of the license at src/LIBSLABLIST.LICENSE
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL HEADER in each
- * file and include the License file at src/LIBSLABLIST.LICENSE.
- * If applicable, add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your own identifying
- * information: Portions Copyright [yyyy] [name of copyright owner]
- *
- * CDDL HEADER END
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 /*
- * Copyright 2014 Nicholas Zivkovic. All rights reserved.
- * Use is subject to license terms.
+ * Copyright (c) 2015, Nick Zivkovic
  */
+
 
 #ifdef UMEM
 #include <umem.h>

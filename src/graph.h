22f60351dfcc3b9b524d83d61deb10b6c52b92ed
Nick Zivkovic <zivkovic.nick@gmail.com>
2015-04-21 01:37:57 +0000

    dfs_rdnt's signature should be in the header.

--- src/graph.h
+++ src/graph.h
@@ -43,4 +43,5 @@ extern int lg_wconnect(lg_graph_t *g, gelem_t e1, gelem_t e2, gelem_t w);
 extern gelem_t lg_bfs_fold(lg_graph_t *g, gelem_t start, adj_cb_t, fold_cb_t, gelem_t z);
 extern gelem_t lg_bfs_rdnt_fold(lg_graph_t *g, gelem_t start, adj_cb_t, fold_cb_t, gelem_t z);
 extern gelem_t lg_dfs_fold(lg_graph_t *g, gelem_t start, pop_cb_t, fold_cb_t, gelem_t z);
+extern gelem_t lg_dfs_rdnt_fold(lg_graph_t *g, gelem_t start, pop_cb_t, fold_cb_t, gelem_t z);
 extern void lg_edges(lg_graph_t *g, edges_cb_t);

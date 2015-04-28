1682e6695a3935a3e0534c1b2cd2043d38c76321
Nick Zivkovic <zivkovic.nick@gmail.com>
2015-04-28 16:54:20 +0000

    Signatures of weighted-graph creation funcs should be in header.

--- src/graph.h
+++ src/graph.h
@@ -37,7 +37,9 @@ typedef void edges_cb_t(gelem_t, gelem_t, gelem_t);
 
 
 extern lg_graph_t *lg_create_graph();
+extern lg_graph_t *lg_create_wgraph();
 extern lg_graph_t *lg_create_digraph();
+extern lg_graph_t *lg_create_wdigraph();
 extern int lg_connect(lg_graph_t *g, gelem_t e1, gelem_t e2);
 extern int lg_wconnect(lg_graph_t *g, gelem_t e1, gelem_t e2, gelem_t w);
 extern gelem_t lg_bfs_fold(lg_graph_t *g, gelem_t start, adj_cb_t, fold_cb_t, gelem_t z);
